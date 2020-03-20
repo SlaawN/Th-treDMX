fb75e1b1e6813bed42d717a84255ff0a0752de39
Robin <robinhenry80132@users.noreply.github.com>
2020-03-20 14:02:21 +0100

    ScrollBar dynamique

---
+++ C++/Robin/Classe Projet/IHM.cpp
@@ -0,0 +1,104 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include "TEditHisto.h"
+#include "IHM.h"
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+    // charge la dll
+	g_dasusbdll=LoadLibrary("DasHard2006.dll");
+	// recupere l'adresse du DMX
+	if(g_dasusbdll)
+	{
+		DasUsbCommand=(DASHARDCOMMAND)::GetProcAddress((HMODULE)g_dasusbdll,"DasUsbCommand");
+	}
+	// ouvre la connexion à l'interface du DMX
+	interface_open=DasUsbCommand(DHC_INIT,0,NULL);
+	DasUsbCommand(DHC_OPEN,0,0);
+
+	//lblResultOpenDMX->Caption=interface_open;
+	// initialise les valeurs a zero
+	for (int i = 0; i < 512; i++) {
+		dmxBlock[i]=0;
+	}
+    // passe les 6 premieres valeurs à 255
+
+
+
+
+	int NbTrackBar=6;
+	for(int i = 0 ; i < NbTrackBar; i++)
+	{
+		TScrollBar * ScrollB = new TScrollBar(this);
+		ScrollB->Parent = this;
+		TEditHisto * edit = new TEditHisto(this, ScrollB);
+		edit->Width=33;
+		edit->Left = i * 50 + 25;
+		ScrollB->Max=255;
+		ScrollB->Left = i * 50 + 30;
+		ScrollB->Top = 50;
+		ScrollB->Tag = i;
+		ScrollB->Max = 255;
+		ScrollB->Position = 255;
+		ScrollB->Kind=sbVertical;
+		ScrollB->OnChange = MyTrackBarChange;
+		dmxBlock[i] = ScrollB->Position;
+		Seq = new Sequence(i,0,dmxBlock[i]);
+	}
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::MyTrackBarChange(TObject *Sender)
+{
+	UnicodeString str = "Bar appelante ";
+	TScrollBar * obj = (TScrollBar*) Sender;
+	str += obj->Tag;
+	str += " valeur ";
+	int Position =255-obj->Position;
+	str += Position;
+	Label1->Caption = str;
+	Seq->setTrame(255-obj->Position, obj->Tag);
+}
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		dmxBlock[i]=Seq->getTrame(i);
+		Memo1->Lines->Add(dmxBlock[i]);
+	}
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::SendTrame()
+{
+	if(interface_open>0)
+	{
+
+		try{
+			DasUsbCommand(DHC_DMXOUT,DMX_MAXCHANNEL,dmxBlock);
+		}
+		catch(int x)
+		{
+		}
+	}
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Timer1Timer(TObject *Sender)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		dmxBlock[i]=Seq->getTrame(i);
+	}
+	SendTrame();
+}
+//---------------------------------------------------------------------------
+
--- C++/Robin/Classe Projet/IHM.h
+++ C++/Robin/Classe Projet/IHM.h
@@ -7,13 +7,39 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include "Sequence.h"
+#include"include/DasHard.h.h"
+#include <sstream>
+#include <deque>
+#include <vector>
+#include <Windows.h>
+#include <string>
+#define DMX_MAXCHANNEL 512
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// Composants gérés par l'EDI
+	//void __fastcall MonEditKeyDown(TObject *Sender, WORD &Key, TShiftState Shift);
+	TLabel *Label1;
+	TMemo *Memo1;
+	TButton *Button1;
+	TTimer *Timer1;
+	void __fastcall MyTrackBarChange(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Timer1Timer(TObject *Sender);
 private:	// Déclarations utilisateur
+	HINSTANCE g_dasusbdll;
+	typedef int(*DASHARDCOMMAND)(int,int,unsigned char*);
+	DASHARDCOMMAND DasUsbCommand;
+	int interface_open;
+	unsigned char dmxBlock[512];
+	Sequence * Seq;
+
 public:		// Déclarations utilisateur
 	__fastcall TForm1(TComponent* Owner);
+		void __fastcall SendTrame();
+
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TForm1 *Form1;
